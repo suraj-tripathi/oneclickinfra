9a9dcaa7176d96883548655b241cafb63a0c494a
Adityarajsingh <thakuraditya20032003@gmail.com>
2025-11-21 22:10:09 +0530

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,54 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "eu-west-2"
+}
+
+variable "owner" {
+  description = "Owner tag"
+  type        = string
+  default     = "abhinav"
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "bastion_subnet_cidr" {
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "redis_master_subnet_cidr" {
+  type        = string
+  default     = "10.0.2.0/24"
+}
+
+variable "redis_replica_subnet_cidr" {
+  type        = string
+  default     = "10.0.3.0/24"
+}
+
+variable "instance_type_bastion" {
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "instance_type_redis" {
+  type        = string
+  default     = "t3.micro"
+}
+
+variable "s3_bucket_name" {
+  description = "S3 bucket for demo (must be globally unique!)"
+  type        = string
+  default     = "abhi-redis-ha-demo-bucket-change-me"
+}
+
+variable "key_name" {
+  description = "Name for AWS key pair"
+  type        = string
+  default     = "redis-demo-key"
+}
