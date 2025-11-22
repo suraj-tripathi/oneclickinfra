55ae1244f796a7dc30ab91f0e5e6aa218f5fa486
suraj-tripathi <surajtripathi167@gmail.com>
2025-11-23 01:06:42 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,24 +1,24 @@
 output "vpc_id" {
-  value = aws_vpc.redis_vpc.id
+  value = aws_vpc.valkey_vpc.id
 }
 
 output "bastion_public_ip" {
   value = aws_instance.bastion.public_ip
 }
 
-output "redis_master_private_ip" {
-  value = aws_instance.redis_master.private_ip
+output "valkey_master_private_ip" {
+  value = aws_instance.valkey_master.private_ip
 }
 
-output "redis_replica_private_ip" {
-  value = aws_instance.redis_replica.private_ip
+output "valkey_replica_private_ip" {
+  value = aws_instance.valkey_replica.private_ip
 }
 
 output "private_key_path" {
   description = "Local path of generated PEM key"
-  value       = local_file.redis_private_key.filename
+  value       = local_file.valkey_private_key.filename
 }
 
 output "s3_bucket_name" {
-  value = aws_s3_bucket.redis_bucket.id
+  value = aws_s3_bucket.valkey_bucket.id
 }
